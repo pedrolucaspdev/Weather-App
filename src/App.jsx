7ab57c7a5d493e7962297d86a5c721fd91a52bce
pedrolucasjdev <pedrolucassec@gmail.com>
2022-06-06 17:48:48 -0300

    adicionando o tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <h1>Testando React!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
